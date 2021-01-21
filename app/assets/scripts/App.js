5684ba73a910c89762ddd846488b7964f091e49f
Aashish Shrestha <43952190+beingaashish@users.noreply.github.com>
2021-01-21 19:38:03 +0545

    Header Navigation completed

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,10 @@
+import { extend } from 'lodash';
 import '../styles/styles.css'
 
 if(module.hot){
     module.hot.accept()
 }
+
+import MobileMenu from './modules/MobileMenu';
+
+let mobileMenu = new MobileMenu();
